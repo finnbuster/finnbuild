e599997dbb8fab842f8f9aebead90cbaa530b803
Finn Robertson <finnbuster@gmail.com>
2015-08-20 17:59:19 +1000

    adding first pass of slick slider

--- js/site.js
+++ js/site.js
@@ -8,4 +8,48 @@ $(function() {
 		url: 'http://finnrobertson.us2.list-manage.com/subscribe/post?u=fea39c610b49b69bbfa0c3a29&id=620444afb9'
 	});
 
+	// Target your .container, .wrapper, .post, etc.
+	$("body").fitVids();
+
+	enquire.register("screen and (max-width:45em)", {
+
+	    // OPTIONAL
+	    // If supplied, triggered when a media query matches.
+	    match : function() {
+	    	console.log('max width 45em');
+	    },      
+	                                
+	    // OPTIONAL
+	    // If supplied, triggered when the media query transitions 
+	    // *from a matched state to an unmatched state*.
+	    unmatch : function() {
+	    	console.log('not max width 45em');
+	    },    
+	    
+	    // OPTIONAL
+	    // If supplied, triggered once, when the handler is registered.
+	    setup : function() {},    
+	                                
+	    // OPTIONAL, defaults to false
+	    // If set to true, defers execution of the setup function 
+	    // until the first time the media query is matched
+	    // deferSetup : true,
+	                                
+	    // OPTIONAL
+	    // If supplied, triggered when handler is unregistered. 
+	    // Place cleanup code here
+	    destroy : function() {}
+	      
+	});
+
+	$('.slick-slider').slick({
+	  prevArrow : '<div class="slick-prev"><div class="sprite sprite-slider-arrow-left hide-mobile"></div><div class="sprite sprite-slider-arrow-left-mobile show-only-mobile"></div></div>',
+	  nextArrow : '<div class="slick-next"><div class="sprite sprite-slider-arrow-right hide-mobile"></div><div class="sprite sprite-slider-arrow-right-mobile show-only-mobile"></div></div>',
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  dots: false,
+	  arrows: true,
+	  appendArrows: $('.slider-arrows .container')
+	});
+
 });
